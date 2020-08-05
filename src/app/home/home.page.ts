701004a100ff9f77b66f290cb15e66c4b45e452a
Khadija Daruwala <khadijadaruwala@Khadijas-MacBook-Pro.local>
2020-08-05 11:18:35 +0530

    Added cordova plugin and created service to use the plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { PluginService } from '../plugin-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  public isRecievedData = false;
+  public reportData: object;
 
-  constructor() {}
+  constructor(public pluginService: PluginService) { }
 
+  add() {
+    this.pluginService.add(5, 3).then(
+      (data) => console.log('Add', data)).catch((error) => console.error(error));
+  }
 }
---
+++ src/app/plugin-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { cordova, IonicNativePlugin } from '@ionic-native/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PluginService extends IonicNativePlugin {
+  static pluginName = 'demoplugin'; // name in package.json file of plugin
+  static plugin = 'cordova-plugin-demoplugin'; // plugin id in the plugin.xml of plugin
+  static pluginRef = 'DemoPlugin';  // clobbers target in the plugin.xml of plugin
+  static repo = 'YOUR_GITHUB_URL';
+  static platforms = ['iOS'];
+
+  add(num1, num2): Promise<any> {
+    const x = cordova(this, 'add', {}, [{ param1: num1, param2: num2 }]);
+    return x;
+  }
+}
